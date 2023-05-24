b4a654781e26f11b52b9917e65997432b18dacf6
Rahul Hidau <Rahul.Hidau@medhost.com>
2023-05-24 15:31:39 +0530

    eureka server added

--- security-service/src/main/java/com/rahul/security/SecurityServiceApplication.java
+++ security-service/src/main/java/com/rahul/security/SecurityServiceApplication.java
@@ -3,10 +3,12 @@ package com.rahul.security;
 import com.rahul.commons.exception.handler.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({GlobalExceptionHandler.class})
+@EnableEurekaClient
 public class SecurityServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityServiceApplication.class, args);
--- user-service/src/main/java/com/rahul/user/UserServiceApplication.java
+++ user-service/src/main/java/com/rahul/user/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.rahul.user;
 import com.rahul.commons.exception.handler.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({GlobalExceptionHandler.class})
+@EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
